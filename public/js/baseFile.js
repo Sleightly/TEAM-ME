3d743c89723f255b2bded6af4d29fc641785e8f4
mrchan <wildcatrobotics2011@gmail.com>
2017-10-21 01:12:42 -0700

    exserver

--- public/js/baseFile.js
+++ public/js/baseFile.js
@@ -1,1 +1,11 @@
 var me = {};
+/*
+att = new WebSocket('wss://ic-websocket-server.run.aws-usw02-pr.ice.predix.io/events');
+att.on('open', function(){
+  att.on('data', function(chunk){
+    console.log(chunk)
+  })
+  att.on('error', function(err){
+    console.log(err);
+  })
+})*/
---
+++ server.js
@@ -0,0 +1,1 @@
+var EXS = require('./lib/expressServer')
