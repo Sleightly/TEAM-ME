780774221239ad1071c90b6183fdc352509962f9
Edward Chen <edward98c@gmail.com>
2017-10-21 19:56:09 -0700

    better icons + bool show

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,8 @@ var map = null;
 function initMap() {
 	var options = {
 		zoom:13,
-		center: {lat:32.7157, lng:-117.1611}
+		center: {lat:32.7157, lng:-117.1611},
+		styles:styles
 	}
 	map = new google.maps.Map(document.getElementById('map'), options);
 
@@ -30,53 +31,67 @@ function initMap() {
 var storage = {};
 var makeMarker = function(locationUiD, coords, type, speed, vol) {
 	var id = {};
+	var bar = {};
 	switch(type) {
 		case "car":
 			id.url = "/images/4wheel.png"
+			id.scaledSize = new google.maps.Size(25, 25);
+			id.origin = new google.maps.Point(0,0);
+			id.anchor = new google.maps.Point(17,26);
+			bar.origin = new google.maps.Point(0,0);
+			bar.anchor = new google.maps.Point(5,vol*2);
 			break;
 		case "human":
 			id.url = "/images/human.png"
+			id.scaledSize = new google.maps.Size(20, 20);
+			id.origin = new google.maps.Point(0,0);
+			id.anchor = new google.maps.Point(15,20);
+			bar.origin = new google.maps.Point(0,0);
+			bar.anchor = new google.maps.Point(5,vol*2);
 			break;
 	}
-	id.scaledSize = new google.maps.Size(20, 20);
-	id.origin = new google.maps.Point(0,0);
-	id.anchor = new google.maps.Point(15,20);
 
-	var bar = {};
 
-	speed = Math.floor(speed/4);
-	if (speed > 15)speed = 15;
-	bar.url = '/images/speed'+speed+'.png';
+	var newSpeed = Math.floor(speed/4);
+	if (newSpeed > 15)newSpeed = 15;
+	bar.url = '/images/speed'+newSpeed+'.svg';
 
 	vol = Math.floor(vol/3*5);
-	bar.scaledSize = new google.maps.Size(25, vol);
+	bar.scaledSize = new google.maps.Size(10, vol);
 
-	bar.origin = new google.maps.Point(0,0);
-	bar.anchor = new google.maps.Point(15,vol);
+	if (type == "human") {
+		vol = Math.floor(vol/2);
+		if (vol > 15)vol = 15;
+		bar.url = '/images/speed'+vol+'.svg';
+	}
 
 	var line = {
-		url: '/images/line.png',
-		scaledSize: new google.maps.Size(50, 7),
+		url: '/images/line.svg',
+		scaledSize: new google.maps.Size(4, 12),
 		origin: new google.maps.Point(0,0),
-		anchor: new google.maps.Point(23, 4)
+		anchor: new google.maps.Point(4,8),
 	}
+
+	var barMarker = new google.maps.Marker({
+		position:coords,
+		icon: bar,
+		map: map,
+		//content: ""
+		zIndex: 0
+	});
 
 	var idMarker = new google.maps.Marker({
 		position:coords,
 		icon: id,
-		map: map
-	});
-	var barMarker = new google.maps.Marker({
-		position:coords,
-		icon: bar,
-		map: map
-		//content: ""
+		map: map,
+		zIndex: 10
 	});
 
 	var lineMarker = new google.maps.Marker({
 		position:coords,
 		icon: line,
-		map: map
+		map: map,
+		zIndex: 20
 	});
 
 	var inforWindow = new google.maps.InfoWindow({
@@ -89,9 +104,11 @@ var makeMarker = function(locationUiD, coords, type, speed, vol) {
 
 
 	storage[locationUiD] = {
-		id: idMarker.icon,
-		bar: barMarker.icon,
-		line: lineMarker.icon
+		coords: coords,
+		type: type,
+		id: idMarker,
+		bar: barMarker,
+		line: lineMarker
 	}
 
 	/*function addMarker(props) {
@@ -118,98 +135,118 @@ var makeMarker = function(locationUiD, coords, type, speed, vol) {
 }
 
 var updateMarker = function(locationUiD, speed, vol) {
-	var bar = {};
-	switch(speed) {
-		case speed < 4:
-			bar.url = "/images/speed15.png"
-			break;
-		case speed < 8:
-			bar.url = "/images/speed14.png"
-			break;
-		case speed < 12:
-			bar.url = "/images/speed13.png"
-			break;
-		case speed < 16:
-			bar.url = "/images/speed12.png"
-			break;
-		case speed < 20:
-			bar.url = "/images/speed11.png"
-			break;
-		case speed < 24:
-			bar.url = "/images/speed10.png"
-			break;
-		case speed < 28:
-			bar.url = "/images/speed9.png"
-			break;
-		case speed < 32:
-			bar.url = "/images/speed8.png"
-			break;
-		case speed < 36:
-			bar.url = "/images/speed7.png"
-			break;
-		case speed < 40:
-			bar.url = "/images/speed6.png"
-			break;
-		case speed < 44:
-			bar.url = "/images/speed5.png"
-			break;
-		case speed < 48:
-			bar.url = "/images/speed4.png"
-			break;
-		case speed < 52:
-			bar.url = "/images/speed3.png"
-			break;
-		case speed < 56:
-			bar.url = "/images/speed2.png"
-			break;
-		case speed < 60:
-			bar.url = "/images/speed1.png"
-			break;
-		case speed > 64:
-			bar.url = "/images/speed0.png"
-			break;
-	}
-	switch(vol) {
-		case vol < 3:
-			bar.scaledSize = new google.maps.Size(20, 5);
-			break;
-		case vol < 6:
-			bar.scaledSize = new google.maps.Size(20, 10);
-			break;
-		case vol < 9:
-			bar.scaledSize = new google.maps.Size(20, 15);
-			break;
-		case vol < 12:
-			bar.scaledSize = new google.maps.Size(20, 20);
-			break;
-		case vol < 15:
-			bar.scaledSize = new google.maps.Size(20, 25);
-			break;
-		case vol < 16:
-			bar.scaledSize = new google.maps.Size(20, 30);
-			break;
-		case vol < 18:
-			bar.scaledSize = new google.maps.Size(20, 35);
-			break;
-		case vol < 21:
-			bar.scaledSize = new google.maps.Size(20, 40);
-			break;
-		case vol < 24:
-			bar.scaledSize = new google.maps.Size(20, 45);
-			break;
-		case vol < 27:
-			bar.scaledSize = new google.maps.Size(20, 50);
-			break;
-		case vol < 30:
-			scaledSize = new google.maps.Size(20, 55);
-			break;
-		case vol >= 30:
-			scaledSize = new google.maps.Size(20, 60);
-			break;
-	}
-	bar.origin = new google.maps.Point(0,0);
-	bar.anchor = new google.maps.Point(0,0);
-	storage[locationUiD].bar = bar;
+	var coords = storage[locationUiD].coords;
+	var type = storage[locationUiD].type;
+	//remove marker
+	storage[locationUiD].bar.setMap(null);
+	storage[locationUiD].id.setMap(null);
+	storage[locationUiD].line.setMap(null);
+	//add new marker
+	makeMarker(locationUiD, coords, type, speed, vol);
+}
+
+var showCars = function(show) {
+	var carmap = null;
+	if(show)carmap = map;
+	Object.keys(storage).forEach(function(key) {
+		if (storage[key].type = 'car') {
+			storage[key].bar.setMap(carmap);
+			storage[key].id.setMap(carmap);
+			storage[key].line.setMap(carmap);
+		}
+	});
+}
+
+var showHuman = function(show) {
+	var hmap = null;
+	if(show)hmap = map;
+	Object.keys(storage).forEach(function(key) {
+		if (storage[key].type = 'human') {
+			storage[key].bar.setMap(hmap);
+			storage[key].id.setMap(hmap);
+			storage[key].line.setMap(hmap);
+		}
+	});
 }
 
 
+var styles = [
+  {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+  {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+  {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#d59563'}]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#d59563'}]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [{color: '#263c3f'}]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#6b9a76'}]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{color: '#38414e'}]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#212a37'}]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#9ca5b3'}]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{color: '#746855'}]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#1f2835'}]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#f3d19c'}]
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [{color: '#2f3948'}]
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#d59563'}]
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [{color: '#17263c'}]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#515c6d'}]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [{color: '#17263c'}]
+  }
+];
